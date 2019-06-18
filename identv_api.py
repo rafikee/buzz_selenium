719bb340da343b36b75ea9fbcea2bcc9074ce89a
Rafik Shoorbajee <rafik.vcu@gmail.com>
2019-06-18 12:01:30 -0400

    display found tokens

--- identv_api.py
+++ identv_api.py
@@ -1,21 +1,30 @@
 import requests
 from dateutil.parser import parse
 import logging
+import re
 
 def get_videos(data):
 
-    base_url = "http://mm.apptek.com:8983/solr/media/select"
-    media_url = "http://mm.apptek.com/media"
-    apptek_url = "http://mm.apptek.com/tv/watch?id="
-    time_concat = ":00Z"
+    base_url = "http://mm.apptek.com:8983/solr/media/select" #URL for sending the GET request
+    media_url = "http://mm.apptek.com/media" # URL prefix that sends us straight to the mp4
+    apptek_url = "http://mm.apptek.com/tv/watch?id=" # URL prefix that sends us to the video in the UI
+    time_concat = ":00Z" # Solr wants datetime to end with this
 
-    channel = data['channel']
-    language = 'ar'
-    query = data['query']
-    results_limit = data['limit']
-    start_date = data['start_date'] + time_concat
-    end_date = data['end_date'] + time_concat
-    query = query.split('\r\n')
+    channel = data['channel'] # Channel parameter as specified by user
+    language = 'ar' # Language code to be passed in GET request
+    results_limit = data['limit'] # limit the number of rows we get in response from IdenTV (specified by user)
+    start_date = data['start_date'] + time_concat # datetime string to be passed to GET request
+    end_date = data['end_date'] + time_concat # datetime string to be passed to GET request
+
+    query = data['query'] # query input by user
+    query = query.split('\r\n') #query takes multiline form, place each segment as item in array
+
+    search_tokens = ' '.join(query)
+    search_tokens = [x for x in search_tokens.split('"') if x and not x.isspace()]
+    search_tokens = '|'.join(search_tokens)
+    search_tokens = search_tokens.replace("*", r"\w*")
+    pattern = re.compile(search_tokens)
+
     for i, line in enumerate(query):
         line = [x for x in line.split('"') if x and not x.isspace()]
         for x, phrase in enumerate(line):
@@ -36,12 +45,24 @@ def get_videos(data):
         'sort' : 'start_dt desc',
         'rows' : results_limit,
         #'fl' : 'start_dt,end_dt,duration,media,' + 'seg_' + language,
-        'fl' : 'start_dt,end_dt,duration,media,',
+        'fl' : 'start_dt,media,seg_ar',
         'fq' : 'start_dt:[' + start_date + ' TO ' + end_date + ']',
     }
 
     r = requests.get(base_url, params = payload)
     data = r.json()
+
+    #[something for segment in clip['seg_ar']]
+
+    payload = [
+        {
+            'video_url' : apptek_url + clip['media'],
+            'tokens' : pattern.findall(' '.join(clip['seg_ar'])),
+        }
+        for clip in data['response']['docs']
+    ]
+
+    #video_urls = [apptek_url + clip['media'] for clip in data['response']['docs']]
 
     ''' If we want MP4 and link to IDENTV
 
@@ -57,4 +78,4 @@ def get_videos(data):
     ''' If we just want link to IDENTV'''
     video_urls = [apptek_url + clip['media'] for clip in data['response']['docs']]
     num_vidoes = data['response']['numFound']
-    return num_vidoes, video_urls
+    return num_vidoes, payload
--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 #use this command to run
 '''flask run -h 0.0.0.0 -p 8080'''
-from flask import Flask, request, jsonify, redirect, render_template
-from get_shares import get_shares
-import requests
-import os
+from flask import Flask, request, redirect, render_template
 from identv_api import get_videos
-import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'You love IIP'
-
-@app.route('/iip', methods=['POST'])
-def iip():
-    if request.headers['token'] == os.environ.get('flask_key'):
-        print("good job you have the right token")
-        shares = get_shares(request.get_json())
-        return jsonify(shares)
-    else:
-        print("intruder alert!")
-        return "Leave me alone!"
 
 @app.route('/cnn')
 def comms():
@@ -40,9 +26,9 @@ def submit_query():
         'channel' : request.form['channel'],
         'limit' : request.form['limit'],
     }
-    num, urls = get_videos(submit_data)
+    num, payload = get_videos(submit_data)
 
-    return render_template('output.html', num=num, urls=urls)
+    return render_template('output.html', num=num, payload=payload)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
